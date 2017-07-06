853472ba51536c6da46540240adf94b0bb12ca04
labasse <labasse@gmail.com>
2017-07-06 09:26:54 +0200

    Change injx method in injxFrom and injxTo methods

--- tests/InjxTest.php
+++ tests/InjxTest.php
@@ -9,22 +9,30 @@ use Injx\Injx;
 class InjxTest extends TestCase {
     public function setUp() {
         $this -> tested = new class { use Injx; };
-        $this -> tested_descendant = (new class { use Injx; }) -> injx( $this->tested );
-        $this -> tested_descendant -> injx( $this->tested );
         $this -> mockContainer = new class {
             function getService($key) {
                 return $key == 'foo' ? 'bar' : NULL;               
             }
         };
     }
+    private function initDescendant() {
+        $this -> tested_descendant = $this->tested->injxTo(
+            new class { use Injx; }
+        );
+        $this -> tested -> injxFrom( $this -> mockContainer );
+    }
     /**
      * @expectedException \InvalidArgumentException
      */
-    public function testInjxBadContainer() {
-        $this -> tested -> injx(new class {});
+    public function testInjxFromBadContainer() {
+        $this -> tested -> injxFrom(new class {});
     }
-    public function testInjxReturnValue() {
-        $actual = $this -> tested -> injx( $this -> mockContainer );
+    public function testInjxToNonContainer() {
+        $anObj = new class {};
+        $this->assertEquals($anObj, $this -> tested -> injxTo($anObj));
+    }
+    public function testInjxFromReturnValue() {
+        $actual = $this -> tested -> injxFrom( $this -> mockContainer );
         $this -> assertEquals($this -> tested, $actual);
     }
     /**
@@ -33,35 +41,41 @@ class InjxTest extends TestCase {
     public function testInjxGetServiceWithoutPreviousInjxCall() {
         $this -> tested -> getService('foo');
     }
-    public function testInjxGetService() {
-        $this -> tested -> injx( $this -> mockContainer );
+    public function testGetService() {
+        $this -> tested -> injxFrom( $this -> mockContainer );
         $this -> assertEquals('bar', $this -> tested -> getService('foo'));
     }
-    public function testInjxGetServiceWithDescendants() {
-        $this -> tested -> injx( $this -> mockContainer );
+    public function testGetServiceWithDescendants() {
+        $this ->initDescendant();
         $actual = $this -> tested_descendant -> getService('foo');
         $this -> assertEquals('bar', $actual);
     }
-    public function testInjxSetServiceWithoutPreviousInjxCall() {
+    public function testSetServiceWithoutPreviousInjxCall() {
         $this -> tested -> setService('foo', 'foobar');
         $this -> assertEquals('foobar', $this -> tested -> getService('foo'));
     }
-    public function testInjxSetService() {
-        $this -> tested -> injx( $this -> mockContainer );
+    public function testSetService() {
+        $this -> tested -> injxFrom( $this -> mockContainer );
         $this -> tested -> setService('bar', 'foo');
         $this -> assertEquals('foo', $this -> tested -> getService('bar'));
     }
-    public function testInjxSetServiceForOverride() {
-        $this -> tested -> injx( $this -> mockContainer );
+    public function testSetServiceReturnValue() {
+        $this -> tested -> injxFrom( $this -> mockContainer );
+        $this->assertEquals(
+            $this -> tested,
+            $this -> tested -> setService('bar', 'foo')
+        );
+    }
+    public function testSetServiceForOverride() {
+        $this -> tested -> injxFrom( $this -> mockContainer );
         $this -> tested -> setService('foo', 'foobar');
         $this -> assertEquals('foobar', $this -> tested -> getService('foo'));
     }
-    public function testInjxSetServiceWithDescendants() {
-        $this -> tested -> injx( $this -> mockContainer );
+    public function testSetServiceWithDescendants() {
+        $this ->initDescendant();
         $this -> tested -> setService('foo', 'foobar');
         $this -> assertEquals('foobar', $this -> tested_descendant -> getService('foo'));
     }
-    private $assert_called;
     private $tested;
     private $tested_descendant;
     private $mockContainer;
